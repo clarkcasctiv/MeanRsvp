37c12cb9258eda8b2439ccb60b96353cc1e42a8a
Pujan Bajra <pujanisme@gmail.com>
2019-03-22 16:36:01 +0545

    added header and footer components

--- server/config.js
+++ server/config.js
@@ -4,6 +4,3 @@ module.exports = {
   MONGO_URI:
     "mongodb+srv://admin:clarkcasctiv@meanrsvp-0hpqi.mongodb.net/test?retryWrites=true"
 };
-
-// "mongodb://bajracharya.pujan23@gmail.com:235N_x9kVD-Liw_TfP@meanrsvp-shard-00-00-0hpqi.mongodb.net:27017/admin"
-// "mongodb://pujan:clarkcasctiv@meanrsvp-shard-00-00-0hpqi.mongodb.net:27017/admin"
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule, Title } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule,],
   providers: [Title],
   bootstrap: [AppComponent]
 })
